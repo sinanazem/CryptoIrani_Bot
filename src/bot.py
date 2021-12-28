a4d094a36328d3344c1a6aa7cd2a5c475d0d51e7
naazem <sinanaazem@gmail.com>
2021-12-28 08:32:41 +0330

    update bot.py file

--- src/bot.py
+++ src/bot.py
@@ -4,24 +4,32 @@ from src.api.api import Crypto
 from loguru import logger
 
 telegram_bot_token = os.environ['TELEGRAMBOT_TOKEN']
-bot = telebot.TeleBot(telegram_bot_token)
-logger.info("Bot start...")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+class Bot():
 
-@bot.message_handler(commands=['doge','DOGE','Doge'])
-def doge_coin(message):
-    logger.info("doge...")
-    a = Crypto('doge')
-    bot.send_message(message.chat.id, f"{a.price()}")
+    def __init__(self):
+        self.bot = telebot.TeleBot(telegram_bot_token)
+        logger.info("start bot...")
+    def run(self):
 
-@bot.message_handler(commands=['btc','BTC','Bitcoin','BITCOIN'])
-def btc_coin(message):
-    logger.info("bitcoin...")
-    a = Crypto('btc')
-    bot.send_message(message.chat.id, f"{a.price()}")
+        @self.bot.message_handler(commands=['start', 'help'])
+        def send_welcome(message):
+            self.bot.reply_to(message, "Howdy, how are you doing?")
 
-bot.infinity_polling()
+        @self.bot.message_handler(commands=['doge','DOGE','Doge'])
+        def doge_coin(message):
+            logger.info("doge...")
+            a = Crypto('doge')
+            self.bot.send_message(message.chat.id, f"{a.price()}")
 
+        @self.bot.message_handler(commands=['btc','BTC','Bitcoin','BITCOIN'])
+        def btc_coin(message):
+            logger.info("bitcoin...")
+            a = Crypto('btc')
+            self.bot.send_message(message.chat.id, f"{a.price()}")
+
+        self.bot.infinity_polling()
+
+if __name__ == "__main__":
+    obj = Bot()
+    obj.run()
